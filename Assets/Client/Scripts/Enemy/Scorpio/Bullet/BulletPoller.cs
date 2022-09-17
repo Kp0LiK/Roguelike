e200a4af2967ec893c628f9fe181f3043ff1d26e
Stanislav <grobovstax@mail.ru>
2022-09-17 23:19:39 +0600

    First commit

---
+++ Assets/Client/Scripts/Enemy/Scorpio/Bullet/BulletPoller.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class BulletPoller : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _lifeTime;
+    [SerializeField] private float _distance;
+    [SerializeField] private int _damage;
+    [SerializeField] private LayerMask _layerMask;
+
+    private void Update()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, _distance, _layerMask);
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.collider.CompareTag("Player"))
+            {
+                hitInfo.collider.GetComponent<HealthManager>().HurtPlayer(_damage);
+            }
+
+            Destroy(gameObject);
+        }
+        transform.Translate(Vector2.up * _speed * Time.deltaTime);
+    }
+}
