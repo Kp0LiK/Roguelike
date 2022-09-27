576fee20cf83480e9fab9551e6f82483592121f9
Stanislav <grobovstax@mail.ru>
2022-09-27 11:00:44 +0600

    Add HeathBar and lives mechanics

---
+++ Assets/Client/Scripts/GameUI/HealthManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HealthManager : MonoBehaviour
+{
+    [SerializeField] private int _currentHealth;
+    private int _maxHealth = 10;
+    public int Health => _currentHealth;
+    public event UnityAction<int> HealthChanged;
+
+    public void HurtPlayer(int damage)
+    {
+        var player = GetComponent<LevelManager>();
+        _currentHealth -= damage;
+        if (_currentHealth <= 0)
+        {
+            _currentHealth = 0;
+            player.Die();
+            _currentHealth = _maxHealth;
+        }
+        UpdateHealth();
+    }
+    
+    public void UpdateHealth() => HealthChanged?.Invoke(_currentHealth);
+    
+    public void HealthAdd(int health)
+    {
+        if (_currentHealth < 10)
+        {
+            _currentHealth += health;
+        }
+        
+        UpdateHealth();
+    }
+}
